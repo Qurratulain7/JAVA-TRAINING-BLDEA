7b29c2863a7995d7b2fbc5228751c5a6b7277821
Qurratulain7 <122641606+Qurratulain7@users.noreply.github.com>
2023-03-20 13:07:01 +0530

    first commit

---
+++ src/main/java/Trainingcodes/test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Trainingcodes;
+
+/**
+ * @author Najmunnisa K
+ *
+ */
+public class test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello every one");
+	}
+
+}
