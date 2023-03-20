821142307b73cdea73fec4ab966d8de3a4111bda
Qurratulain7 <122641606+Qurratulain7@users.noreply.github.com>
2023-03-21 00:05:42 +0530

    second commit

---
+++ src/main/java/Trainingcodes/program1.java
@@ -0,0 +1,21 @@
+package Trainingcodes;
+/**
+ * 
+ */
+
+/**
+ * @author Najmunnisa K
+ *
+ */
+public class program1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("muskan");
+		// TODO Auto-generated method stub
+
+	}
+
+}
